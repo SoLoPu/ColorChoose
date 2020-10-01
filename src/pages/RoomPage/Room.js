4ff142f424dac0fa9b869c7efc2f36adf1923ad6
long nguyễn ngọc thanh <longnnt99@gmail.com>
2020-10-01 16:06:01 +0700

    add update room

--- src/pages/RoomPage/Room.js
+++ src/pages/RoomPage/Room.js
@@ -1,18 +1,8 @@
 import React, { Component, useEffect, useState } from 'react';
 import RoomComponent from "./RoomCoponent"
 import fire from "../../FireBase"
-import 'firebase/firestore';
 
 export default function Room(props) {
-    const dataListRoom = [
-        { name: "room1" },
-        { name: "room2" },
-        { name: "room3" },
-        { name: "room4" },
-        { name: "room5" },
-        { name: "room6" },
-        { name: "room7" },
-    ]
     const [listRoom, setListRoom] = useState([])
     useEffect(() => {
         const db = fire.firestore();
