8dd8b24ac3d4c98dce661ad43885eadbe8e03a74
SoLoPu <phuvinhhung1999@gmail.com>
2020-10-01 13:43:59 +0700

    add start and gameplay

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -6,7 +6,11 @@ export default class Start extends Component {
     render(){
         return(
             <div className="about-wrapper">
-                <h1>Start</h1>
+                <h1>Funny Color</h1>
+                <form action="POST">
+                   <input type="text" placeholder="User name" />
+                   <input type="submit" value="Submit"/>
+                </form>
             </div>
 
         )
