b8838e58442201433508341b9ae1290a03ab6588
SoLoPu <phuvinhhung1999@gmail.com>
2020-10-01 21:15:29 +0700

    add Gameplay

--- src/pages/GamePlay.js
+++ src/pages/GamePlay.js
@@ -4,29 +4,17 @@ import Shape from "../component/Shape";
 import dataColor from "../data/dataColor"
 import dataShape from "../data/dataShape"
 
-const randomColor = (n) => {
-    let colorArr = [];
-    for(let i = 0; i < n; i++){
-        let color = dataColor[Math.floor(Math.random()*dataColor.length)];
-        // let shape = dataShape[Math.floor(Math.random()*dataShape.length)]
-        if(colorArr.includes(color)){
-            i--;
-        }
-        else{
-            colorArr.push({color:color, shape:'square'});
-            // colorArr.push({color:color, shape:shape})
-        }
-        
-    }
-    console.log(colorArr);
-    return colorArr;
-}
+import fire from "../FireBase";
+import { firestore } from 'firebase';
+
+
 
 
 export default class GamePlay extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            host: false,
             round: 1,
             time: 5,
             player1: 0,
@@ -38,23 +26,27 @@ export default class GamePlay extends Component {
                 "yellow",
                 "black"
             ],
-            shapes: [
-            ]
+            question: ""
         }
         this.countDown = this.countDown.bind(this);
+        this.onClickStart = this.onClickStart.bind(this);
+        this.randomColor =this.randomColor.bind(this);
     }
+
+    
+    
     render() {
         return (
             <div className="about-wrapper">
                 <h1>RoomName {this.state.round}</h1>
                 <p className="count-down">{this.state.time}</p>
                 {this.state.active &&
-                <div className="result" style={{backgroundColor:this.state.colors[Math.floor(Math.random()*4)].color}}></div>}
+                <div className="result" style={{backgroundColor:this.state.question}}></div>}
                 <div className="score">
 
                     <div className="player">
                         <h3>Player 1</h3>
-                        <p>{this.state.player2} win</p>
+                        <p>{this.state.player1} win</p>
                     </div>
                     <div className="player">
                         <h3>Player 2</h3>
@@ -65,10 +57,19 @@ export default class GamePlay extends Component {
                 <div className="container">
                     {
                         this.state.colors.map(item => 
-                            <Shape shape={!this.state.active ? item.shape : "square"} color={!this.state.active ?  item.color : "white"} style={!this.state.active? {} : {border:"1px solid black"}} key={item.color} />
+                            <Shape shape={!this.state.active ? item.shape : "square"} 
+                                color={!this.state.active ?  item.color : "white"} 
+                                style={!this.state.active? {} : {border:"1px solid black"}} 
+                                question={this.state.question} time={this.state.time} 
+                                key={item.color} 
+                                keyColor={item.color}
+                                host={this.state.host}
+                            />
                         )
                     }
                 </div>
+                
+                <button onClick={this.onClickStart}>Start</button>
 
             </div>
 
@@ -76,42 +77,130 @@ export default class GamePlay extends Component {
         
     }
 
+    onClickStart(){
+    
+        
+        this.setState({
+            active: false,
+            host:true,
+            time: 5,
+            colors: []
+        }, function(){
+            this.countDown();
+            this.setState({
+            colors: this.randomColor(4)
+        })
+        })
+        
+
+    }
+    
+
+    randomColor = (n) => {
+        
+        let colorArr = [];
+        for(let i = 0; i < n; i++){
+            let color = dataColor[Math.floor(Math.random()*dataColor.length)];
+            // let shape = dataShape[Math.floor(Math.random()*dataShape.length)]
+            
+            let found= false;
+            for(let i = 0; i<colorArr.length; i++){
+                if(colorArr[i].color===color){
+                    found = true
+                }
+            }
+            if(found!== false){
+                i--;
+            }
+            else{
+                colorArr.push({color:color, shape:'square'});
+                // colorArr.push({color:color, shape:shape})
+            }
+            
+        }
+        
+        let question= colorArr[Math.floor(Math.random()*4)].color;
+        this.setState({
+            question: question
+        })
+
+        const db = fire.firestore();
+        db.collection("rooms").doc("3dGkXKxwc7WYy5rA1b1s").set({
+            colors: colorArr,
+            question: question,
+            time: 5,
+            active: false
+        }, { merge: true })
+        .then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+        
+        return colorArr;
+    }
 
     countDown(){
+        let countDownTime = 10;
         let component = this;
-        // Set the date we're counting down to
-      
 
-        // Update the count down every 1 second
         var x = setInterval(function() {
+            countDownTime-=1;
+            
 
+
+        const db = fire.firestore();
+        if(component.state.time !== 0){
+            db.collection("rooms").doc("3dGkXKxwc7WYy5rA1b1s").set({
+                time: component.state.time-1
+    
+            }, { merge: true })
+            .then(function() {
+                console.log("Document successfully written!");
+            })
+            .catch(function(error) {
+                console.error("Error writing document: ", error);
+            });
+    
+
+        }
         
-       component.setState({
-            time: component.state.time - 1
-       })
 
         // If the count down is finished, write some text
         if (component.state.time <= 0) {
-            component.setState({
-                time: 0
-           });
-           let arrWhite = component.state.colors;
-           
-           component.setState({
-                active:true
-           })
+
+            db.collection("rooms").doc("3dGkXKxwc7WYy5rA1b1s").set({
+                active: true,
+                time: 0   
+            }, { merge: true })
+        }
+        
+        if(countDownTime <= 0){
+            component.onClickStart();
             clearInterval(x);
-            
         }
         }, 1000);
     }
 
     componentDidMount(){
-        this.countDown();
-        this.setState({
-            colors: randomColor(4)
-        })
-
+        const component = this;
+        const db = firestore();
+        db.collection("rooms").doc("3dGkXKxwc7WYy5rA1b1s")
+        .onSnapshot(function(doc) {
+            component.setState({
+                colors: doc.data().colors,
+                time: doc.data().time,
+                active: doc.data().active,
+                question: doc.data().question,
+                player1: doc.data().users[0].point,
+                player2: doc.data().users[1].point,
+                
+            })
+            console.log("Current data: ", doc.data());
+        });
+        
+        
     }
 
     
