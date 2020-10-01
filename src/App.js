1078772177c191b1e382edc3c29adc1a036c8ace
long nguyễn ngọc thanh <longnnt99@gmail.com>
2020-10-01 11:31:29 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+
 
 function App() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyA89-IMEI0PP8YB3H5zWqMkg3ZvTRWoGiQ",
+    authDomain: "beaming-theorem-269908.firebaseapp.com",
+    databaseURL: "https://beaming-theorem-269908.firebaseio.com",
+    projectId: "beaming-theorem-269908",
+    storageBucket: "beaming-theorem-269908.appspot.com",
+    messagingSenderId: "1035141791361",
+  };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
   return (
     <div className="App">
       <header className="App-header">
